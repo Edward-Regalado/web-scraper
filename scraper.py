f2ad30408d9c4d258e48389ad61d51de7c0e3141
Edward Regalado <tregalado30@gmail.com>
2021-08-02 19:48:56 -0700

    added in-class headings function

--- scraper.py
+++ scraper.py
@@ -22,5 +22,23 @@ def get_citations_needed_report(URL):
         print(f'{list}. Citations needed for report: \n {x.parent.text}')
         # print(f'Citations needed for report: \n {list}')
 
+## Added in-class heading example
+def get_citation_headings(URL):
+    headings = []
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    anchors = soup.find_all('a')
+    heading_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+    for anchor in anchors:
+        text = anchor.get_text()
+        if 'citation needed' in text:
+            element = anchor.parent.parent.parent
+            for ps in element.previous_siblings:
+                if ps.name in heading_tags:
+                    headings.append(ps.text)
+                    break
+    return headings
+
 print(get_citations_needed_count(URL))
 print(get_citations_needed_report(URL))
+print(get_citation_headings(URL))
