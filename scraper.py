7169a0740e3f8b66beab7ae223dbbf6216a5dde4
Edward Regalado <tregalado30@gmail.com>
2021-08-02 18:39:46 -0700

    finsihed functions, added readme

--- scraper.py
+++ scraper.py
@@ -1,16 +1,26 @@
-import request
+import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://en.wikipedia.org/wiki/History_of_Mexico'
+URL = 'https://en.wikipedia.org/wiki/History_of_Mexico'
+# page = requests.get(URL)
+# print(page.content)
 
 def get_citations_needed_count(URL):
-    response = request.get(URL)
-    soup = BeautifulSoup(response.text,'html.parser')
-      
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content,'html.parser')
+    citations = soup.find_all(class_="noprint Inline-Template Template-Fact")
+    return f'\n The number of Citations needed is: {len(citations)} \n'
 
 def get_citations_needed_report(URL):
-    response = request.get(URL)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    citations = soup.find_all(class_="noprint Inline-Template Template-Fact")
+    list = 0
+    for x in (citations):
+        list += 1
+        print(f'{list}. Citations needed for report: \n {x.parent.text}')
+        # print(f'Citations needed for report: \n {list}')
 
+print(get_citations_needed_count(URL))
+print(get_citations_needed_report(URL))
